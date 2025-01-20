df671078230fea7693aa0391554483bd2a5f1432
Alesia Via <alesiavia@outlook.com>
2025-01-20 12:32:52 +0300

    refactor:burger

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const menuElement = document.querySelector('.nav_list');
 const overlayElement = document.querySelector('.overlay');
 const linkMenuElement =document.querySelectorAll('.nav-link');
 
+const closeMenu = () => {
+    menuElement.classList.remove("nav_list_is_open");
+    overlayElementy.classList.remove("overlay_is-visible");
+    bodyElement.classList.remove("body_no-scroll");
+  };
 function toggleMenu(){
     burgerElement.classList.toggle('burger_active');
     bodyElement.classList.toggle('body_no-scroll');
@@ -14,10 +19,26 @@ burgerElement.addEventListener('click',() =>{
     toggleMenu();
 });
 overlayElement.addEventListener('click',()=>{
-    toggleMenu();
+    closeMenu();
 });
 linkMenuElement.forEach((link)=>{
     link.addEventListener('click',()=>{
-        toggleMenu(); 
+        closeMenu(); 
     })
 })
+// switcher language
+
+// Добавляем обработчик события на изменение языка
+document.getElementById('language-select').addEventListener('change', function () {
+    const selectedLanguage = this.value; // Получаем выбранный язык: "eng" или "ru"
+    setLanguage(selectedLanguage);
+});
+
+// Функция для изменения языка
+function setLanguage(lang) {
+    // Находим все элементы с атрибутами data-eng и data-ru
+    document.querySelectorAll('[data-eng]').forEach(el => {
+        // Меняем текст элемента на значение атрибута data-lang
+        el.textContent = el.getAttribute(`data-${lang}`);
+    });
+}
